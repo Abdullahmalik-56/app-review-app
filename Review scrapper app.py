1ab9498fa0da448f249a5dcda641d31ed56182f2
Muhammad Abdullah Malik <i.abdullahmalik2@gmail.com>
2025-09-10 01:27:50 +0500

    Add files via upload

--- Review scrapper app.py
+++ Review scrapper app.py
@@ -6,7 +6,7 @@ import requests
 url = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 token="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" 
 
-keyword=['best cricket game','cricket game','cricket real game']
+
 def user_menu():
     print("\nAvailable actions:")
     print(" - search   : Search and save apps")
@@ -44,45 +44,47 @@ def llm(prompt):
 # Get apps from Google Play
 def search_apps_by_keywords():
     all_apps = []
-    for key in keyword:
-        app_names = search(key, country='us', lang='en')
-        all_apps.extend(app_names)
+    app_names = search(keyword, country='us', lang='en')
+    all_apps.extend(app_names)
     return all_apps
 
 # Write app titles and IDs to file
 def save_app_list(apps):
-    with open("new.txt", "w") as f:
+    with open("Nasearchmes.txt", "w") as f:
         for app_info in apps:
             f.write(f"{app_info['title']} : {app_info['appId']}\n")
 # Get reviews of apps
-def fetch_reviews (app_ids,app_titles):
+def fetch_reviews (app_ids,app_titles,user_choice):
     all_reviews = {}
+    if user_choice =='keywords': 
 
-    for app_id, app_title in zip(app_ids, app_titles):
-        print(f"Fetching reviews for {app_title}...")
-        result, _ = reviews(app_id, country='us', lang='en', sort=Sort.NEWEST, count=100)
-        contents = [review['content'] for review in result if 'content' in review]
-        all_reviews[app_title] = contents
-        with open("new.txt", "w") as f:
-                f.write(f"{all_reviews}")
-
+        for app_id, app_title in zip(app_ids, app_titles):
+            print(f"Fetching reviews for {app_title}...")
+            result, _ = reviews(app_id, country='us', lang='en', sort=Sort.NEWEST, count=100)
+            contents = [review['content'] for review in result if 'content' in review]
+            all_reviews[app_title] = contents
+    else:
+        result, _ = reviews(keyword, country='us', lang='en', sort=Sort.NEWEST, count=100)
+        all_reviews.update(result)
     return all_reviews
-
 # Write reviews to file
 def save_reviews (app_titles, all_reviews):
     with open("reviews.txt", "w") as f:
         for name in app_titles:
             f.write(f"--------------------------------------- {name} ---------------------------------------\n")
-        for review in all_reviews:
-            for k, v in review.items():
+            for k, v in all_reviews.items():
                 if k not in ['reviewId', 'userImage']:
                     f.write(f"{k}: {v}\n")
 
 # Get details of apps
-def fetch_app_details (app_ids):
+def fetch_app_details (app_ids,user_choice):
     app_details = []
-    for app_id in app_ids:
-        result = app(app_id, country='us', lang='en')
+    if user_choice =='keywords':
+        for app_id in app_ids:
+            result = app(app_id, country='us', lang='en')
+            app_details.append(result)
+    else:
+        result = app(keyword, country='us', lang='en')
         app_details.append(result)
     return app_details
 
@@ -111,45 +113,47 @@ def summarize_reply_content(all_reviews):
             f.write(f"{summary_response}\n")
 
                 
-#Get reply percentile
-#def rextract_reply_data(app_ids):
- ##
- #   reply_data=fetch_reviews(app_ids)
-  #  for reply_info in reply_data:
-   #     for k,v in reply_info.items():
-    #        if k in ['content']:reply.append(v)
-    
-                
-                
-    
-
-
-
 # Main driver
 if __name__ == "__main__":
+    while True:
+        user_choice=input('Keywords or app id : ').lower()
+        if user_choice =='keywords':
+            keyword=input('Give a keyword : ')
+            break
+        elif user_choice =='app id':
+            keyword = input('give app id : ')
+            break
+        else:
+            print('Invalid choice')
+    
+
     all_apps = search_apps_by_keywords()
     app_ids, app_titles = zip(*[(app['appId'], app['title']) for app in all_apps])
-    
     while True:
         action = user_menu()
         if action == 'search':
             save_app_list(all_apps)
+            print('Done')
         elif action == 'reviews':
-            all_reviews = fetch_reviews(app_ids, app_titles)
+            all_reviews = fetch_reviews(app_ids, app_titles,user_choice)
             save_reviews(app_titles, all_reviews)
+            print('Done')
         elif action == 'details':
-            app_details = fetch_app_details(app_ids)
+            app_details = fetch_app_details(app_ids,user_choice)
             save_app_details(app_titles, app_details)
+            print('Done')
         elif action == 'summary':
-            all_reviews = fetch_reviews(app_ids, app_titles)
+            all_reviews = fetch_reviews(app_ids, app_titles,user_choice)
             summarize_reply_content(all_reviews)
+            print('Done')
         elif action == 'all':
             save_app_list(all_apps)
-            all_reviews = fetch_reviews(app_ids, app_titles)
+            all_reviews = fetch_reviews(app_ids, app_titles,user_choice)
             save_reviews(app_titles, all_reviews)
-            app_details = fetch_app_details(app_ids)
+            app_details = fetch_app_details(app_ids,user_choice)
             save_app_details(app_titles, app_details)
             summarize_reply_content(all_reviews)
+            print("All actions completed.")
         elif action == 'quit':
             print("Exiting program.")
             break
